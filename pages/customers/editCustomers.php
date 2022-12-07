70bf96999f4963da0d8b493622a51c809a097363
ramonluteijn <ramon.luteijn@rocdeleijgraaf.nl>
2022-12-07 15:53:36 +0100

    leden gefixt

--- pages/customers/editCustomers.php
+++ pages/customers/editCustomers.php
@@ -7,25 +7,26 @@
         { 
             //update functie 
             (new Customer($customer->getId(), $_POST["name"], $_POST["email"], $_POST["phone"], Status::getStatusByVarchar($_POST["status"])->getId()))->updateCustomer();
-            $msg ="Uw status is gewijzigd naar: ".$_POST["status"].".\rUw inlog mail:".$_POST["email"]."\rBezoek de website: http://localhost/DeBlauweLoper/home";
-            mail($email, "Wijziging status",$msg, "From:info@deblauweloper.nl"); 
+
+            $msg ="Uw status is gewijzigd naar: ".$_POST["status"].".\rUw inlog mail:" .$_POST["email"]."\rBezoek de website: http://localhost/DeBlauweLoper/home";
+            mail($_POST["email"], "Wijziging status",$msg, "From:info@deblauweloper.nl"); 
+
             header("Location: ". ROOT . "/customers/customers");
             
         }
     ?>
-
     <section class="text-center mt-5">
         <h2>Leden aanpassen</h2>
     </section>
 <!-- from leden aanpassen -->
     <div class="container mt-5">
-        <form  method="post">
+        <form method="post">
             <label>Naam:</label>
-            <input type="text" name="name" value="<?=ucwords($customer->getName())?>" disabled>
+            <input type="text" name="name" value="<?=$customer->getName()?>" readonly>
             <label>Email:</label>
-            <input type="email" name="email" value="<?=$customer->getEmail()?>" disabled>
+            <input type="email" name="email" value="<?=$customer->getEmail()?>" readonly>
             <label>Telefoonnummer:</label>
-            <input type="text" name="phone" value="<?=$customer->getPhone()?>" disabled>
+            <input type="text" name="phone" value="<?=$customer->getPhone()?>" readonly>
             <label>Status:</label>
             <input type="text" list="status" name="status" required>
             <datalist id="status">
